532b3341823bb145da1a904311763e9b73418146
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-07-24 11:27:03 +0200

    js: localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -16,27 +16,59 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
-
 const addGlass = document.querySelector('.add-glass');
 const removeGlass = document.querySelector('.remove-glass');
 const reset = document.querySelector('.reset');
 const glassCounter = document.querySelector('.glass__counter');
+const key = new Date().toISOString().slice(0, 10);
 
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  glassCounter.innerHTML = '0';
+}
+else {
+  glassCounter.innerHTML = localStorage.getItem(key);
+}
 
+// ROZWIĄZANIE Z KURSU:
 addGlass.addEventListener('click', (e) => {
-  for (let i = glassCounter.textContent; i < 10; i) {
-    glassCounter.innerHTML = ++i;
-    break;
+  const currentValue = parseInt(localStorage.getItem(key));
+  if (currentValue <= 12) {
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) +1);
+    glassCounter.innerHTML = parseInt(localStorage.getItem(key));
   }
+});
+removeGlass.addEventListener('click', (e) => {
+  const currentValue = parseInt(localStorage.getItem(key));
+  if (currentValue > 0) {
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) -1);
+    glassCounter.innerHTML = localStorage.getItem(key);
+  };
 });
 
-removeGlass.addEventListener('click', (e) => {
-  for (let i = glassCounter.textContent; i > 0; i) {
-    glassCounter.innerHTML = --i;
-    break;
-  }
-});
+
+
+
+// MOJE ROZWIĄZANIE Z PĘTLĄ FOR:
+// addGlass.addEventListener('click', (e) => {
+//   for (let i = glassCounter.textContent; i < 10; i) {
+//     glassCounter.innerHTML = ++i;
+//     localStorage.setItem(key, parseInt(glassCounter.textContent));
+//     break;
+//   }
+// });
+
+// removeGlass.addEventListener('click', (e) => {
+//   for (let i = glassCounter.textContent; i > 0; i) {
+//     glassCounter.innerHTML = --i;
+//     localStorage.setItem(key, parseInt(glassCounter.textContent));
+//     break;
+//   }
+// });
 
 reset.addEventListener('click', (e) => {
   glassCounter.innerHTML = 0;
+  localStorage.setItem(key, 0);
 })
+
+
